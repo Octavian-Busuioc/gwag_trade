f8019ec89024482d475b37abee5a9c224ca28367
Octavian Busuioc <octavianbusuioc184@yahoo.ro>
2023-03-27 12:46:47 +0300

    fetch data + modal

--- js/script.js
+++ js/script.js
@@ -21,32 +21,33 @@ document.onclick = function (e) {
 
 
 
+
 // open modal
 
-var modal = document.getElementById('simpleModal');
+// var modal = document.getElementById('simpleModal');
 
-var modalBtn = document.getElementById('modalBtn');
+// var modalBtn = document.getElementById('modalBtn');
 
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
-
-
-// listen for click 
-
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
+// var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
 
+// // listen for click 
 
-// function to open modal
+// modalBtn.addEventListener('click', openModal);
+// closeBtn.addEventListener('click', closeModal);
 
-function openModal() {
-  modal.style.display = "block";
-}
-// function to close the modal
 
-function closeModal() {
-  modal.style.display = 'none';
-}
+// // function to open modal
+
+// function openModal() {
+//   modal.style.display = "block";
+// }
+// // function to close the modal
+
+// function closeModal() {
+//   modal.style.display = 'none';
+// }
+
 
 
 
